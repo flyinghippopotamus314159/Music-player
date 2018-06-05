4027594d827e81b34af2a86558ea72e9937762cc
flyinghippopotamus314159 <h1409637@trbvm.com>
2018-06-05 19:35:29 +0100

    Update and rename music player.py to beat player.py

--- beat player.py
+++ beat player.py
@@ -1,10 +1,5 @@
 import winsound, time
 tune=input("Enter a beat code:")
-##step_size=int(input("Enter step size:"))
-##bpm=int(input("Enter bpm:"))
-##bps=bpm/60
-##sps=bps/step_size
 for i in range (len(tune)):
     sound=str(tune[i])
     winsound.PlaySound(sound, winsound.SND_FILENAME)
-##    time.sleep(sps)
